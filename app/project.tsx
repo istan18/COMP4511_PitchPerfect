fe127c8c1edd45cccfba83afe09b0d8744dea346
Christian Tolentino <z5420628@ad.unsw.edu.au>
2024-11-04 14:34:44 +1100

    Added colours

---
+++ app/project.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+const NewProject = () => {
+  return <SafeAreaView>New Project</SafeAreaView>;
+};
+
+export default NewProject;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundGreen: '#171F20',
+        textGray: '#ABABAB',
+      },
+    },
   },
   plugins: [],
-}
+};
