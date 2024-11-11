d3a7be404cb202f596a77ca56bded51f16d7ee4e
Ahmad Jawad Bakhtiari <aj.bakhti@gmail.com>
2024-11-12 08:38:26 +1100

    Added Members

--- app/(tabs)/project/project-overview.tsx
+++ app/(tabs)/project/project-overview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { View, Text, ScrollView, SafeAreaView, Image } from "react-native";
 import ImageSlider from "@/components/ImageSlider";
 import ProjectHeader from "@/app/(tabs)/project/projectHeader";
 
@@ -8,6 +8,29 @@ const ProjectOverview = () => {
     require("@/assets/images/meal-planner-2.png"),
     require("@/assets/images/meal-planner-1.png"),
     require("@/assets/images/meal-planner-3.png"),
+  ];
+
+  const members = [
+    {
+      role: "Developer",
+      icon: require("@/assets/images/jennyProfile.png"),
+    },
+    {
+      role: "Designer",
+      icon: require("@/assets/images/emptyProfile.png"),
+    },
+    {
+      role: "Manager",
+      icon: require("@/assets/images/emptyProfile.png"),
+    },
+    {
+      role: "Tester",
+      icon: require("@/assets/images/emptyProfile.png"),
+    },
+    {
+      role: "Support",
+      icon: require("@/assets/images/kevinProfile.png"),
+    },
   ];
 
   return (
@@ -23,6 +46,21 @@ const ProjectOverview = () => {
             socialise over lunch, this app provides a seamless experience for
             organising group meals on campus.
           </Text>
+          <View className="border-b border-gray-500 mt-3" />
+          <Text className="px-5 pt-2 text-2xl text-white text-center font-extrabold">
+            Members
+          </Text>
+          <View className="flex-row justify-around mt-4 px-5">
+            {members.map((member, index) => (
+              <View key={index} className="items-center">
+                <Image
+                  source={member.icon}
+                  className="w-16 h-16 rounded-full"
+                />
+                <Text className="text-white mt-2">{member.role}</Text>
+              </View>
+            ))}
+          </View>
           <View className="border-b border-gray-500 mt-3" />
         </View>
       </ScrollView>
