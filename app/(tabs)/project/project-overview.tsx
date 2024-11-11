db2d89345ad42bb9709878a255738e9535604c72
Ahmad Jawad Bakhtiari <aj.bakhti@gmail.com>
2024-11-11 20:47:32 +1100

    Added changes

--- app/(tabs)/project/project-overview.tsx
+++ app/(tabs)/project/project-overview.tsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import ImageSlider from "@/components/ImageSlider";
 import ProjectHeader from "@/app/(tabs)/project/projectHeader";
 
 const ProjectOverview = () => {
   const images = [
-    "https://picsum.photos/1024/767",
-    "https://picsum.photos/1024/768",
-    "https://picsum.photos/1024/769",
+    require("@/assets/images/meal-planner-2.png"),
+    require("@/assets/images/meal-planner-1.png"),
+    require("@/assets/images/meal-planner-3.png"),
   ];
 
   return (
-    <ScrollView className="flex-1 bg-[#171F20]">
+    <View className="flex-1">
       <ProjectHeader projectName="Meal Planner" />
-      <View className="">
-        <ImageSlider images={images} height={250} />
-        <Text className="p-4 mt-4 text-base text-gray-600">
-          Welcome to the project overview page. Here you will find all the
-          details about the project.
-        </Text>
-      </View>
-    </ScrollView>
+      <ScrollView className="flex-1 bg-[#171F20]">
+        <View className="mt-5">
+          <ImageSlider images={images} height={250} />
+          <Text className="px-5 pt-3 text-xl text-white">
+            This app is designed to help students coordinate meal plans, form
+            dining groups, and discover the best on-campus dining spots. Whether
+            students are looking for a quick meal between classes or a chance to
+            socialise over lunch, this app provides a seamless experience for
+            organising group meals on campus.
+          </Text>
+          <View className="border-b border-gray-500 mt-3" />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -4,15 +4,15 @@ import { View, Image, FlatList, Dimensions } from "react-native";
 const { width } = Dimensions.get("window");
 
 interface ImageSliderProps {
-  images: string[];
+  images: number[];
   height: number;
 }
 
 export default function ImageSlider({ images, height }: ImageSliderProps) {
-  const renderItem = ({ item }: { item: string }) => (
+  const renderItem = ({ item }: { item: number }) => (
     <View style={{ width: width * 0.85, marginHorizontal: 10 }}>
       <Image
-        source={{ uri: item }}
+        source={item}
         style={{ width: "100%", height: "100%", borderRadius: 20 }}
         resizeMode="cover"
       />
